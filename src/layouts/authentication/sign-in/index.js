e62442bfe55f7d6d156098e772ed5fd7857ea46b
HamzaWebPro <hamza.mern2201@gmail.com>
2023-08-22 11:07:16 +0600

    text deploy

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -93,7 +93,7 @@ function SignIn() {
 
   return (
     <CoverLayout
-      title="Welcome back"
+      title="Welcome back to"
       description="Enter your email and password to sign in"
       image={curved9}
     >
